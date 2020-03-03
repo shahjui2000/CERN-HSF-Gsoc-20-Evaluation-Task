27b72c12062babe4c94146fe89615f0cc9668ea5
Jui Shah <34505328+shahjui2000@users.noreply.github.com>
2020-03-03 22:20:52 +0530

    Update root2array.py

--- root2array.py
+++ root2array.py
@@ -14,23 +14,17 @@ print(data.ls())
 
 sig = data.Get('sig_tree;2')
 bkg = data.Get('bkg_tree;2')
-i = 0
 
 signal = np.zeros((1,1024))
 for event in sig:
     signal = np.vstack((signal, np.vstack(sig.vars).T))   
-    # i = i+1
-    # if(i==10): 
-    #     break
 signal = signal[1:,:]
 print(signal.shape)
-i = 0
+
+
 background = np.zeros((1,1024))
 for event in bkg:
     background = np.vstack((background, np.vstack(bkg.vars).T))
-    # i = i+1
-    # if(i==10): 
-    #     break
 background = background[1:,:]
 print(background.shape)
 
